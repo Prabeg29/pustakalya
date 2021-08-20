c60a685360811bc3ac3e8fc1ee01ad70817c44a2
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-08-20 12:29:58 +0545

    BookCRUD: Set models

---
+++ app/Models/Book.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create(array $array)
+ * @method static chunk(int $int, \Closure $param)
+ * @method static where(string $string, string $string1, int $id)
+ */
+class Book extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'cover_image',
+        'qty'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class);
+    }
+
+    /**
+     * @param $collection
+     */
+    public function commaSeparate($collection)
+    {
+        $arr = [];
+        foreach($collection as $object)
+        {
+            $arr[] = $object["name"];
+        }
+        return implode(",", $arr);
+    }
+}
---
+++ app/Models/Genre.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static firstOrCreate(array $array)
+ */
+class Genre extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function books()
+    {
+        return $this->belongsToMany(Book::class);
+    }
+}
