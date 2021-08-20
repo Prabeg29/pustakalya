ce83164d3a1ac88c5b34060b128b71539a15f9e5
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-08-20 12:31:34 +0545

    BookCRUD: Define resource

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "description" => $this->when($request->book, $this->description),
+            "coverImage" => $this->cover_image,
+            "authors" => $this->commaSeparate($this->authors),
+            "genres" => $this->commaSeparate($this->genres),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
