ee5b4b6dc3ed8280728248de3bbf1af0fb16fa95
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-08-24 16:29:45 +0545

    Authorize user delete action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,10 +77,12 @@ class UserController extends Controller
      * Remove the specified resource from storage.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $this->authorize('delete', $user);
+        $user->delete();
+        return response()->json([], 204);
     }
 }
